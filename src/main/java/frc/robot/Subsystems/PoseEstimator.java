ea3d8517b4ae60014904e0621019335ac1da2abe
Paul Seed <pseed@abilisne.com>
2024-02-08 18:17:41 -0500

    pathplanner lib update

--- src/main/java/frc/robot/Subsystems/PoseEstimator.java
+++ src/main/java/frc/robot/Subsystems/PoseEstimator.java
@@ -16,8 +16,6 @@ import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.trajectory.Trajectory;
