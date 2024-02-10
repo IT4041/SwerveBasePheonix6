af156e11cbacb6ef603465914b38b7dac238c94e
Hortonbear <148155624+Hortonbear@users.noreply.github.com>
2024-02-10 12:16:21 -0500

    Firing Head Code

--- src/main/java/frc/robot/Subsystems/FiringHead.java
+++ src/main/java/frc/robot/Subsystems/FiringHead.java
@@ -2,16 +2,56 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class FiringHead extends SubsystemBase {
-  /** Creates a new FiringHead. */
-  public FiringHead() {}
+  
+  private CANSparkMax fireMotor;
+  private CANSparkMax followMotor;
 
+  private CANSparkMax transportMotor;
+
+  
+
+
+  
+  /** Creates a new FiringHead. */
+  public FiringHead() {
+    fireMotor = new CANSparkMax(Constants.FiringHeadConstants.UpperSparkmaxDeviceID,MotorType.kBrushless);
+    followMotor = new CANSparkMax(Constants.FiringHeadConstants.LowerSparkmaxDeviceID,MotorType.kBrushless);
+    fireMotor.restoreFactoryDefaults();
+    followMotor.restoreFactoryDefaults();
+    
+    transportMotor = new CANSparkMax(Constants.FiringHeadConstants.UpperTransportSparkmaxDeviceID,MotorType.kBrushless);
+    transportMotor.restoreFactoryDefaults();
+
+    fireMotor.setIdleMode(IdleMode.kBrake);
+    fireMotor.setSmartCurrentLimit(80);
+    fireMotor.setClosedLoopRampRate(1);
+
+    followMotor.setIdleMode(IdleMode.kBrake);
+    followMotor.setSmartCurrentLimit(80);
+    followMotor.setClosedLoopRampRate(1);
+
+    transportMotor.setIdleMode(IdleMode.kBrake);
+    transportMotor.setSmartCurrentLimit(80);
+    transportMotor.setClosedLoopRampRate(1);
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  public void fire() {
+    
   }
 }
